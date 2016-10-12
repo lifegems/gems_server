205b7c9c54b9c5b0e47406524a574770ff1269eb
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-10-12 09:16:28 -0400

    Add https endpoint

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-var express = require('express');
-var cors = require('cors');
+var express    = require('express');
 var bodyParser = require('body-parser');
+var cors       = require('cors');
+var fs         = require('fs');
+var https      = require('https');
 
+// Key and Client
+var key  = fs.readFileSync('./certs/gems-key.pem');
+var cert = fs.readFileSync('./certs/gems-cert.pem');
+
+// setup server options
+var https_opts = {
+  key: key,
+  cert: cert
+};
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Gather all routes
 var URL = 'mongodb://localhost:4200/gems';
 var routes = require('./api/routes.js');
 
@@ -21,7 +33,8 @@ routes.forEach(function(route) {
 
 });
 
-app.listen(443, function() {
-  console.log("listening on port 443");
+// Initiate server
+var httpsServer = https.createServer(https_opts, app);
+httpsServer.listen(443, function() {
+  console.log("Listening to https://localhost:443");
 });
-
