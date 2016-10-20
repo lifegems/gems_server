b3c4677cf67bbf5b1727b2f22ca4bd2134b2812a
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-10-20 12:39:23 -0400

    Add new bible routes

--- api/routes.js
+++ api/routes.js
@@ -2,39 +2,9 @@ var insight = require('./insight/insight.controller.js');
 var mongo = require('mongodb').MongoClient;
 var DB_URL = "mongodb://localhost:4200/gems";
 var https = require('https');
+var fs    = require('fs');
 
 module.exports = [
-  {
-    path: '/api/bible',
-    request: 'get',
-    callback: function(req,res) {
-      https.get('https://www.jw.org/en/publications/bible/nwt/books/json/html/', function(response) {
-        var data = "";
-        response.on('data', function(chunk) {
-          data += chunk;
-        });
-        response.on('end', function() {
-          res.send(data);
-        });
-      });
-    }
-  },
-  {
-    path: '/api/bible/:range',
-    request: 'get',
-    callback: function(req,res) {
-      var url = 'https://www.jw.org/en/publications/bible/nwt/books/json/html/' + req.params.range;
-      https.get(url, function(response) {
-        var data = "";
-        response.on('data', function(chunk) {
-          data += chunk;
-        });
-        response.on('end', function() {
-          res.send(data);
-        });
-      });
-    }
- },
  {
     path: '/api/terms',
     request: 'get',
@@ -71,16 +41,50 @@ module.exports = [
     }
   },
   {
-    path: '/api/nwt/books',
+    path: '/api/bible',
     request: 'get',
     callback: function(req,res) {
-      mongo.connect(DB_URL, function(err, db) {
-        db.collection('BibleBooks').find().toArray(function(err, docs) {
-          res.send(docs);
-          db.close();
+      var contents = fs.readFileSync('../data/nwt/books/books.json');
+      res.send(contents);
+    }
+  },
+  {
+    path: '/api/bible/:book/:chapter',
+    request: 'get',
+    callback: function(req,res) {
+      var contents = fs.readFileSync('../data/nwt/books/books.json');
+      res.send(contents);
+    }
+  },
+  {
+    path: '/api/bible/jw/',
+    request: 'get',
+    callback: function(req,res) {
+      https.get('https://www.jw.org/en/publications/bible/nwt/books/json/html/', function(response) {
+        var data = "";
+        response.on('data', function(chunk) {
+          data += chunk;
+        });
+        response.on('end', function() {
+          res.send(data);
         });
       });
     }
-  }
-
+  },
+  {
+    path: '/api/bible/jw/:range',
+    request: 'get',
+    callback: function(req,res) {
+      var url = 'https://www.jw.org/en/publications/bible/nwt/books/json/html/' + req.params.range;
+      https.get(url, function(response) {
+        var data = "";
+        response.on('data', function(chunk) {
+          data += chunk;
+        });
+        response.on('end', function() {
+          res.send(data);
+        });
+      });
+    }
+ }
 ];
---
+++ scripts/downloadBibleBooks.js
@@ -0,0 +1,64 @@
+var https = require('https');
+var fs   = require('fs');
+
+https.get('https://www.jw.org/en/publications/bible/nwt/books/json/html/', function(response) {
+  var data = "";
+  response.on('data', function(chunk) {
+    data += chunk;
+  });
+  response.on('end', function() {
+    writeContentsToFile('../data/nwt/books.json', data);
+    downloadAllBooks(data);
+  });
+});
+
+
+function getBookContents(bookid, chapter) {
+   var url = 'https://www.jw.org/en/publications/bible/nwt/books/json/html/';
+   var rng = formatRange(bookid, chapter);
+   
+   https.get(url + rng, function(response) {
+     var data = "";
+     response.on('data', function(chunk) {
+       data += chunk;
+     });
+     response.on('end', function() {
+       var filename = bookid + '.' + chapter + '.json';
+       writeContentsToFile('../data/nwt/books/' + filename, data);
+     });
+   });
+}
+
+function writeContentsToFile(filepath, contents) {
+   if (fs.exists(filepath)) {
+      fs.unlink(filepath);
+   }
+   fs.writeFile(filepath, contents, { flag: 'w' });
+   console.log('Saved file: ' + filepath);
+}
+
+function downloadAllBooks(bookdata) {
+   bookdata = JSON.parse(bookdata);
+   for (var i = 0; i < 66; i++) {
+      var bookid = i + 1;
+      var book = bookdata.editionData.books[bookid];
+      var chapters = parseInt(book.chapterCount);
+      for (var j = 0; j < chapters; j++) {
+         var chapter = j + 1;
+         getBookContents(bookid, chapter);
+      }
+   }
+}
+
+function formatRange(bookid, chapterid) {
+   var chapter = "";
+   if (chapterid < 10) {
+      chapter = "00" + chapterid;
+   } else if (chapterid < 100) {
+      chapter = "0" + chapterid;
+   } else {
+      chapter = chapterid;
+   }
+
+   return bookid + chapter + "001-" + bookid + chapter + "999";
+}
