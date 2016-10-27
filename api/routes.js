1e7406ce0d1c466dc22a80249720ffb5fcb2a044
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-10-26 23:06:24 -0400

    Fix formatting

--- api/routes.js
+++ api/routes.js
@@ -20,7 +20,8 @@ module.exports = [
    {
       path: '/api/terms',
       request: 'get',
-      callback: function(req,res) {var search = {};
+      callback: function(req,res) {
+         var search = {};
          if (req.query.search) {
             var searchTerm = RegExp(["(", req.query.search, ")+"].join(""), "gi");
             search = {
@@ -46,58 +47,56 @@ module.exports = [
     path: '/api/terms/:term',
     request: 'get',
     callback: function(req,res) {
-      mongo.connect(DB_URL, function(err, db) {
-        var searchTerm = RegExp(["^", req.params.term, "$"].join(""), "i");
-        db.collection('terms').find({'name': searchTerm }).toArray(function(err, docs) {
-          res.send(docs);
-          db.close();
-        });
+      var searchTerm = RegExp(["^", req.params.term, "$"].join(""), "i");
+      var search = {
+         name: searchTerm
+      };
+      dbc.select('terms', search, function(data) {
+         res.send(data);
       });
     }
   },
   {
-    path: '/api/it',
-    request: 'get',
-    callback: function(req,res) {
+   path: '/api/it',
+   request: 'get',
+   callback: function(req,res) {
       mongo.connect(DB_URL, function(err, db) {
-        var search = {};
-        if (req.query.search) {
-          var searchTerm = RegExp(["(", req.query.search, ")+"].join(""), "gi");
-          search = {
-            name: searchTerm
-          };
-        }
-        db.collection('articles').find(search).toArray(function(err, docs) {
-          res.send(docs);
-          db.close();
-        });
+         var search = {};
+         if (req.query.search) {
+            var searchTerm = RegExp(["(", req.query.search, ")+"].join(""), "gi");
+            search = {
+               name: searchTerm
+            };
+         }
+         dbc.select('articles', search, function(data) {
+            res.send(data);
+         });
+       });
+      }
+   },
+   {
+   path: '/api/it/:term',
+   request: 'get',
+   callback: function(req,res) {
+      var searchTerm = RegExp(["^", req.params.term, "$"].join(""), "i");
+      var search = {
+         name: searchTerm
+      };
+      dbc.select('articles', search, function(data) {
+         res.send(data);
       });
-    }
-  },
-  {
-    path: '/api/it/:term',
-    request: 'get',
-    callback: function(req,res) {
-      mongo.connect(DB_URL, function(err, db) {
-        var searchTerm = RegExp(["^", req.params.term, "$"].join(""), "i");
-        db.collection('articles').find({'name': searchTerm }).toArray(function(err, docs) {
-          res.send(docs);
-          db.close();
-        });
-      });
-    }
-  },
-  {
-    path: '/api/notes',
-    request: 'post',
-    callback: function(req, res) {
-      mongo.connect(DB_URL, function(err, db) {
-        db.collection('notes').insertOne(req.body);
-        db.close();
-      });
-    }
-  },
-  {
+   },
+   {
+      path: '/api/notes',
+      request: 'post',
+      callback: function(req, res) {
+         mongo.connect(DB_URL, function(err, db) {
+            db.collection('notes').insertOne(req.body);
+            db.close();
+         });
+      }
+   },
+   {
     path: '/api/notes/:book/:chapter',
     request: 'get',
     callback: function(req,res) {
@@ -108,53 +107,53 @@ module.exports = [
         });
       });
     }
-  },
-  {
-    path: '/api/bible',
-    request: 'get',
-    callback: function(req,res) {
-      var contents = fs.readFileSync('data/nwt/books.json');
-      res.send(JSON.parse(contents));
-    }
-  },
-  {
-    path: '/api/bible/:book/:chapter',
-    request: 'get',
-    callback: function(req,res) {
-      var file = 'data/nwt/books/' + req.params.book + '.' + req.params.chapter + '.json';
-      var contents = fs.readFileSync(file);
-      res.send(JSON.parse(contents));
-    }
-  },
-  {
-    path: '/api/bible/jw/',
-    request: 'get',
-    callback: function(req,res) {
-      https.get('https://www.jw.org/en/publications/bible/nwt/books/json/html/', function(response) {
-        var data = "";
-        response.on('data', function(chunk) {
-          data += chunk;
-        });
-        response.on('end', function() {
-          res.send(data);
-        });
-      });
-    }
-  },
-  {
-    path: '/api/bible/jw/:range',
-    request: 'get',
-    callback: function(req,res) {
-      var url = 'https://www.jw.org/en/publications/bible/nwt/books/json/html/' + req.params.range;
-      https.get(url, function(response) {
-        var data = "";
-        response.on('data', function(chunk) {
-          data += chunk;
-        });
-        response.on('end', function() {
-          res.send(data);
-        });
-      });
-    }
- }
+   },
+   {
+      path: '/api/bible',
+      request: 'get',
+      callback: function(req,res) {
+         var contents = fs.readFileSync('data/nwt/books.json');
+         res.send(JSON.parse(contents));
+      }
+   },
+   {
+      path: '/api/bible/:book/:chapter',
+      request: 'get',
+      callback: function(req,res) {
+         var file = 'data/nwt/books/' + req.params.book + '.' + req.params.chapter + '.json';
+         var contents = fs.readFileSync(file);
+         res.send(JSON.parse(contents));
+      }
+   },
+   {
+      path: '/api/bible/jw/',
+      request: 'get',
+      callback: function(req,res) {
+         https.get('https://www.jw.org/en/publications/bible/nwt/books/json/html/', function(response) {
+            var data = "";
+            response.on('data', function(chunk) {
+               data += chunk;
+            });
+            response.on('end', function() {
+               res.send(data);
+            });
+         });
+      }
+   },
+   {
+      path: '/api/bible/jw/:range',
+      request: 'get',
+      callback: function(req,res) {
+         var url = 'https://www.jw.org/en/publications/bible/nwt/books/json/html/' + req.params.range;
+         https.get(url, function(response) {
+            var data = "";
+            response.on('data', function(chunk) {
+               data += chunk;
+            });
+           response.on('end', function() {
+             res.send(data);
+           });
+         });
+      }
+   }
 ];
